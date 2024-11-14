fe43f6d7b77782787ee872027d3ec43a7ce68afb
Oliver Sharp <olliesharp5@gmail.com>
2024-11-14 20:54:47 +0000

    Added visual validation to register form

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,11 +9,19 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password']
 
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        # Make email, first_name, last_name explicitly required
+        self.fields['email'].required = True
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['role', 'about']
-    
+
     def __init__(self, *args, **kwargs):
         super(UserProfileForm, self).__init__(*args, **kwargs)
         self.fields['role'].choices = [(tag[0], tag[1]) for tag in UserProfile.USER_ROLES if tag[0] in ['CG', 'EU']]
+        self.fields['about'].required = False
