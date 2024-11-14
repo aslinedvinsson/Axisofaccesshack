21a2282296f05ebbb3ef19927432c57adabea2ff
Oliver Sharp <olliesharp5@gmail.com>
2024-11-14 17:49:21 +0000

    Wired up base template

--- axisofaccessproject/settings.py
+++ axisofaccessproject/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'axisofaccessproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
