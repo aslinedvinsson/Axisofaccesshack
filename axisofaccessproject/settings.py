46949423fce6eae80dda474e0d027687707ca085
farhatamannaislam <farha.islam@students.codeinstitute.net>
2024-11-15 11:57:47 +0100

    Change: settings for development

--- axisofaccessproject/settings.py
+++ axisofaccessproject/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&#aw$zs5&29xklsvd9dio0--xe&emuo_n)vlb(*qf=*((b2)8p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "*"
