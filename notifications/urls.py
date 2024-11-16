ab58a3976820f5db9d2e877c5e720c5f922b0996
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-11-16 17:50:37 +0000

    First notification ding iteration

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import  send_notification, notification_index
+from .views import  send_notification, notification_index, unread_notification_count
 
 urlpatterns = [
     path('notifications/', notification_index, name='notification_index'),
     path('send-notification/<int:icon_id>/', send_notification, name='send_notification'),
+    path('unread_notifications/', unread_notification_count, name='unread_notifications'),
 ]
 
 
